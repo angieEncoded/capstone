d32600c4bc76074230a46e349d8dddae2f5aeaa7
angieEncoded <43992078+angieEncoded@users.noreply.github.com>
2021-12-18 12:56:18 -0500

    Fix another bug with the edit button not showing back up on cancel

--- tabicrm/static/tabicrm/javascript/editCustomer.js
+++ tabicrm/static/tabicrm/javascript/editCustomer.js
@@ -86,6 +86,8 @@ const submitForm = async (event, id, fieldName) => {
 
 // Close the small editing form
 const cancelEdit = (fieldName, currentData) => {
+    const editButton = document.querySelector(`#edit-${fieldName}-icon`)
+    editButton.style.display = "block"
     document.querySelector(`#edit-${fieldName}`).innerHTML = currentData
 }
 
