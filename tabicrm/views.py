b6fed62b513d15bbbc076d7f3de4b5848562a811
angieEncoded <43992078+angieEncoded@users.noreply.github.com>
2021-12-22 16:53:56 -0500

    get the form auto populate working

--- tabicrm/views.py
+++ tabicrm/views.py
@@ -336,12 +336,39 @@ def download_license(request, id):
 
 def customer_full_form(request, id):
 
+    # Get the customer's initial form values
     customer = Customer.objects.get(id = id)
-    customerForm = forms.Customer()
-    return render(request, "tabicrm/customer_full_form.html", {
-        "customer": customer,
+    customerForm = forms.newCustomerForm(initial={
+        'name': customer.name,
+        'primary_phone': customer.primary_phone,
+        'secondary_phone': customer.secondary_phone,
+        'fax': customer.fax,
+        'website': customer.website,
+        'notes': customer.notes,
+        'billing_address_one': customer.billing_address_one,
+        'billing_address_two': customer.billing_address_two,
+        'billing_address_city': customer.billing_address_city, 
+        'billing_address_state': customer.billing_address_state,
+        'billing_address_zip': customer.billing_address_zip,
+        'billing_address_country': customer.billing_address_country,
+        'shipping_address_one': customer.shipping_address_one,
+        'shipping_address_two': customer.shipping_address_two,
+        'shipping_address_city': customer.shipping_address_city, 
+        'shipping_address_state': customer.shipping_address_state,
+        'shipping_address_zip': customer.shipping_address_zip,
+        'shipping_address_country': customer.shipping_address_country
     })
-    pass
+
+    # Get the list of licenses 
+
+
+    # Get the list of contacts
+
+    return render(request, "tabicrm/customer_full_form.html", {
+        "form": customerForm,
+    })
+
+
 
 
 
